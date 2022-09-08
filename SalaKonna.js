a4391161dc35ef19dfb8147a06386963a466a77b
Marko Tenhivaara <marko.tenhivaara@gmail.com>
2022-09-08 12:54:02 +0300

    First commit

---
+++ SalaKonna.js
@@ -0,0 +1,30 @@
+const { Client, GatewayIntentBits, Partials, ActivityType } = require('discord.js')
+const client = new Client({
+    intents: [ /*https://discord-api-types.dev/api/discord-api-types-v10/enum/GatewayIntentBits */
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildPresences
+    ],
+    partials: [ /*https://discord.js.org/#/docs/discord.js/14.0.3/typedef/Partials */
+        Partials.Message,
+        Partials.User,
+        Partials.Channel,
+        Partials.GuildMember
+    ]
+});
+
+require('dotenv').config();
+
+client.on('ready', () =>{
+    console.log(`Logged in as ${client.user.tag}`)
+
+    client.user.setActivity(`B`,{
+        type: ActivityType.Listening
+    });
+
+    client.user.setStatus('online')
+});
+
+
+client.login(process.env.TOKEN)
