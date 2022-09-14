f90764bbaf0feb01dfe8d22ec72473095453cdc0
Julia Söderlund <julia.soderlund@yandex.ru>
2022-09-14 15:43:49 +0300

    registered slash commands

--- SalaKonna.js
+++ SalaKonna.js
@@ -1,4 +1,6 @@
-const { Client, GatewayIntentBits, Partials, ActivityType } = require('discord.js')
+const { REST } = require('@discordjs/rest');
+const { Client, GatewayIntentBits, Partials, ActivityType, Routes } = require('discord.js')
+
 const client = new Client({
     intents: [ /*https://discord-api-types.dev/api/discord-api-types-v10/enum/GatewayIntentBits */
         GatewayIntentBits.Guilds,
@@ -15,6 +17,11 @@ const client = new Client({
 });
 
 require('dotenv').config();
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -23,9 +30,25 @@ client.on('ready', () => {
         name: `how to develop a bot :)`,
         type: ActivityType.Watching
     });
-
     client.user.setStatus('online')
 });
 
+async function main() {
+    const commands = [
+        {
+            name: "help",
+            description: "How to get started with the bot"
+        }
+    ];
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+            body: commands,
+        });
+        client.login(TOKEN);
+    } catch (err) {
+        console.log(err);
+    }
+  }
 
-client.login(process.env.TOKEN)
+main();
