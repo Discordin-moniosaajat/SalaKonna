59c5aa681cfadaa73e321bc44a341663d92efc91
Woopsoops <parviainen.joni@gmail.com>
2022-10-12 15:19:05 +0300

    forget models folder

---
+++ models/test.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create schema and model for testing
+const testSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    tdata: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+const Test = mongoose.model('Test', testSchema)
+module.exports = Test;
