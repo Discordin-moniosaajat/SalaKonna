782aa3d879f7ad56ad3da48c6d9db81fe1eba9b3
Woopsoops <parviainen.joni@gmail.com>
2022-10-27 10:12:30 +0300

    pseudoname schema

--- models/pseudoname.js
+++ models/pseudoname.js
@@ -2,3 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Create schema and model for pseudoname
+
+const PseudonameSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+const PseudonamePart1 = mongoose.model('Pseudoname1', pseudonameSchema)
+module.exports = PseudonamePart1;
