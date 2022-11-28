2a657b79d301fe92f4a3a8d00d00ec28a36e25ca
Marko Tenhivaara <marko.tenhivaara@gmail.com>
2022-11-28 20:15:00 +0200

    /help command updated

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,10 @@ const helpEmbed = new EmbedBuilder()
     .setTitle('Help')
     .addFields(
         { name: 'User manual: ', 
-          value: '> Locate a text channel where you can find a button to create a ticket. Clicking it will create you a private text channel which only you and the moderators can see. From this private text channel you can use /post command to send messages to a public text channel under a pseudonym. ' 
+          value: '> Type /post command to send messages to a public text channel under a pseudonym. If your message contains sensitive content you can first use the /toggletw command to switch to a different text channel where your message will be sent.' 
         },
         { name: 'List of commands: ', 
-          value: '/help - gives you a user manual on how to use the bot \n /post - sends your messages to a text channel under a pseudonym' 
+          value: '/help - gives you a user manual on how to use the bot \n /post - sends your messages to a text channel under a pseudonym \n /toggletw - changes the channel where your anonymous message will be sent' 
         },
     );
 
